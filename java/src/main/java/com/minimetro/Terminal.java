65e6195e04f9ab6e2af32ac89ffd5f5f1af591cc
Josh Dale <j.dale38@gmail.com>
2014-03-07 13:20:32 -0700

    init commit for prototype

---
+++ java/src/main/java/com/minimetro/Terminal.java
@@ -0,0 +1,75 @@
+package com.minimetro;
+
+import java.util.Random;
+import java.util.UUID;
+
+import static java.lang.Integer.MAX_VALUE;
+
+public class Terminal {
+    private enum Type {
+        SQUARE, CIRCLE, HOUSE, PLUS, TRIANGLE;
+
+        static Type randomTerminal() {
+            int index = random.nextInt();
+            int size = values().length;
+
+            return values()[index % size];
+        }
+    }
+
+    static {
+        random = new Random(MAX_VALUE);
+    }
+
+    private final Type type;
+    private final UUID uuid;
+
+    private static final Random random;
+
+
+    public Terminal() {
+        this(Type.randomTerminal());
+    }
+
+    public Terminal(Type type) {
+        this.type = type;
+
+        uuid = UUID.randomUUID();
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    @Override
+    public String toString() {
+        return "Terminal{" +
+            "type=" + type +
+            ", uuid=" + uuid +
+            '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Terminal terminal = (Terminal) o;
+
+        if (type != terminal.type) return false;
+        if (uuid != null ? !uuid.equals(terminal.uuid) : terminal.uuid != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = type != null ? type.hashCode() : 0;
+        result = 31 * result + (uuid != null ? uuid.hashCode() : 0);
+        return result;
+    }
+}
---
+++ java/src/test/java/com/minimetro/TerminalTest.java
@@ -0,0 +1,22 @@
+package com.minimetro;
+
+import junit.framework.TestCase;
+
+import org.junit.Assert;
+
+public class TerminalTest extends TestCase {
+    private Terminal terminal;
+
+    public void setUp() throws Exception {
+        terminal = new Terminal();
+    }
+
+    public void testTerminalHasType() {
+        Assert.assertNotNull(terminal.getType());
+    }
+
+    public void testTerminalHasUuid() {
+        Assert.assertNotNull(terminal.getUuid());
+    }
+
+}
